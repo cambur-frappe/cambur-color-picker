af61f6e60749068fd492b66d1db251d5904beae4
David Pérez <taborlindp@gmail.com>
2024-07-31 13:54:03 +0200

    initial setup

--- src/cambur-color-picker.ts
+++ src/cambur-color-picker.ts
@@ -1,81 +1,27 @@
-import { LitElement, html, css } from 'lit';
-import { property, customElement } from 'lit/decorators.js';
+import { LitElement, html, css } from "lit";
+import { TWStyles } from "./tailwind/twlit";
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
-
-@customElement('cambur-color-picker')
 export class CamburColorPicker extends LitElement {
-  @property({ type: String }) header = 'My app';
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--cambur-color-picker-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
+  static styles = TWStyles;
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/CamburColorPicker.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        id="wrapper"
+        class="flex justify-between flex-col items-center h-full"
+      >
+        <div>Hola Mundo!</div>
+        <canvas class="border border-black" id="canvas" width="150" height="150"></canvas>
+        <footer
+          id="footer"
+          class="flex justify-center items-center w-full h-12 bg-gray-100"
         >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+          Made with 💙 by Frappé Team 🍧
+        </footer>
+      </div>
     `;
   }
 }
+
+customElements.define("cambur-color-picker", CamburColorPicker);
